1f0df283a381e18dbdd4c39fe93cea49a2ebb825
Keena <Keena@MacBook-Pro-de-Keena.local>
2014-11-19 13:50:47 +0100

    modifications de petites erreurs

--- connection_bdd.php
+++ connection_bdd.php
@@ -5,11 +5,8 @@
  * Date: 21/10/14
  * Time: 11:25
  */
-try
-{
-    $bdd = new PDO('mysql:host=localhost;dbname=maisondesligues', 'root', ''); //On essaye de se connecter à la bdd
-}
-catch (Exception $e)
-{
-    die('Erreur : ' . $e->getMessage()); // Si la connection ne passe pas, on renvoie un message d'erreur et tout s'arrête
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=maisondesligues', 'root', '');
+} catch (Exeption $e) {
+    die('Erreur:' . $e->getMessage());
 }
