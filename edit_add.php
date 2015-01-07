c8668a73478c5ddca6e64f051a597ce643395c6d
Keena <Keena@MacBook-Pro-de-Keena.local>
2015-01-07 17:04:46 +0100

    Edit added

---
+++ edit_add.php
@@ -0,0 +1,31 @@
+<?php
+$getid = $_POST['id'];
+$newTitle = $_POST['newTitle'];
+$newDetails = $_POST['newDetails'];
+include "connection_bdd.php";
+
+$update=$bdd->prepare("UPDATE adds
+                        SET title=:newTitle,
+                        Details=:newDetails
+                        WHERE id = :getid");
+$update->bindParam(':newTitle',$newTitle);
+$update->bindParam(':newDetails',$newDetails);
+$update->bindParam(':getid',$getid);
+$update->execute();
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>ADD</title>
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
+</head>
+<body>
+<form action="edit_add.php" method="post">
+    <input type="text" id="newTitle" value="<?=$_POST['title']?>"/>
+    <input type="text" id="newDetails" value="<?=$_POST['Details']?>"/>
+    <button>Envoyer</button>
+</form>
+</body>
+</html>
--- post_add.php
+++ post_add.php
@@ -20,9 +20,9 @@ include('connection_bdd.php');
 if(isset($_POST['title'])){
     echo $_POST['title'];
     echo $_POST['describe'];
-    $post = $bdd->prepare("INSERT INTO adds (title, Details) VALUES (?, ?) "); //Insérer en base de données depuis les input
-    $post->bindParam(1, $_POST['title']);// On définit le premier ?
-    $post->bindParam(2, $_POST['describe']);// On définit le deuxième ?
+    $post = $bdd->prepare("INSERT INTO adds (title, Details) VALUES (:setTitle, :setDetails) "); //Insérer en base de données depuis les input
+    $post->bindParam(':setTitle', $_POST['title']);// On définit le premier ?
+    $post->bindParam(':setDetails', $_POST['describe']);// On définit le deuxième ?
     $add = $post->execute();// On execute la requête
 
 }
@@ -37,11 +37,11 @@ if(isset($_POST['title'])){
     <link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
 </head>
 <body>
-    <form method="post">
-        Titre : <input type="text" name="title"/>
-        Image : <input type="text" name="upload"/>
-        Description : <input type="text" name="describe"/>
-        <input type="submit" value="submit" class="btn btn-primary"/>
-    </form>
+<form method="post">
+    Titre : <input type="text" name="title"/>
+    Image : <input type="text" name="upload"/>
+    Description : <input type="text" name="describe"/>
+    <input type="submit" value="submit" class="btn btn-primary"/>
+</form>
 </body>
 </html>
