84884a2fa1853d281ff26e5c83c06a15cc61a8b3
Thomas <thomas.leux2710@gmail.com>
2015-01-14 11:33:56 +0100

    MAJ module de recherche

--- module_recherche.php
+++ module_recherche.php
@@ -22,139 +22,120 @@ include("connection_bdd.php");
     <link rel="icon" href="./bootstrap/favicon.ico">
     <link rel="stylesheet" type="text/css" href="bootstrap\css\slider.css">
     <title>Jumbotron Template for Bootstrap</title>
-
-    <!-- Bootstrap core CSS -->
     <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="./bootstrap/jumbotron.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+	<link href="./bootstrap/jumbotron.css" rel="stylesheet">
 </head>
+	<body>
 
-<body>
+		<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		    <div class="container">
+		        <div class="navbar-header">
+		            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+		                <span class="sr-only">Toggle navigation</span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		            </button>
+		            <a class="navbar-brand" href="#">Maison des Ligues Lorraines</a>
+					<form method="post" action="module_recherche.php">
+				        <input type="text" name="recherche"/>
+				        <input type="submit" value="Envoyer"/>
+				    </form>
 
-<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Maison des Ligues Lorraines</a>
-		<form method="post" action="module_recherche.php">
-	        <input type="text" name="recherche"/>
-	        <input type="submit" value="Envoyer"/>
-	    </form>
+		        </div>
+		        <div class="navbar-collapse collapse">
+		            <form class="navbar-form navbar-right" role="form">
+		                <div class="form-group">
+		                    <input type="text" placeholder="Email" class="form-control">
+		                </div>
+		                <div class="form-group">
+		                    <input type="password" placeholder="Password" class="form-control">
+		                </div>
+		                <button type="submit" class="btn btn-success">Sign in</button>
+		            </form>
+		        </div>
+		        
+		    </div>
+		</div>
+
+		<div class="jumbotron">
+		    <div class="container" id="tableau">
+		    	<div class="table">
+			    	<?php
+						foreach ($resultat as $key) {
+							//echo $key['title'].','.$key['pseudo'].','.$key['detail'];
+						?>
+							<table id="tableau" class="table">
+								<tr>
+										<?php if (isset($key['title'])):?>
+									<td><strong>Titre</strong></td>
+										<?php endif?>
+										<?php if (isset($key['pseudo'])):?>
+									<td><strong>Pseudo</strong></td>
+										<?php endif?>
+										<?php if (isset($key['detail'])):?>
+									<td><strong>Details</strong></td>
+										<?php endif?>
+								</tr>
+								<tr>	<?php if (isset($key['title'])):?>
+									<td><?php echo $key['title'];?></td>
+										<?php endif?>
+										<?php if (isset($key['pseudo'])):?>
+									<td><?php echo $key['pseudo'];?></td>
+										<?php endif?>
+										<?php if (isset($key['detail'])):?>
+									<td><?php echo $key['detail'];?></td>
+										<?php endif?>
+								</tr>
+							</table>
+						<?php
+						}
+						?>
+				</div>
+			</div>
+		</div> 	
+
+		<!--<div class="container">
+	    
+	    	<div class="row">
+		        <div class="col-md-4">
+		            <h2>Heading</h2>
+		            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+		            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+		        </div>
+		        <div class="col-md-4">
+		            <h2>Heading</h2>
+		            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+		            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+		        </div>
+		        <div class="col-md-4">
+		            <h2>Heading</h2>
+		            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+		            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+	        	</div>
+	    	</div>
+	    </div>-->
+
+	    
+
+	    <!--<footer>
+	        <p>&copy; Company 2014</p>
+	    </footer>-->
+	
 	</body>
 
-        </div>
-        <div class="navbar-collapse collapse">
-            <form class="navbar-form navbar-right" role="form">
-                <div class="form-group">
-                    <input type="text" placeholder="Email" class="form-control">
-                </div>
-                <div class="form-group">
-                    <input type="password" placeholder="Password" class="form-control">
-                </div>
-                <button type="submit" class="btn btn-success">Sign in</button>
-            </form>
-        <!--</div><!--/.navbar-collapse 
-        	<div class="container">
-        		<ul class="nav nav-dropdown-2nd-col navbar-right">
-        			<li><a href="#" class="carret">Se connecter</a></li>
-        			<ul class="carret"></ul>
-        				<li><a href="#">S'inscrire</a></li>
-        				<li><a href="#">Test</a></li>
-        				<li><a href="#">Blabla</a></li>
-        		</ul>
-        	</div>-->
-    </div>
-</div>
+	<!-- Bootstrap core JavaScript
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
 
-<!-- Main jumbotron for a primary marketing message or call to action -->
-<div class="jumbotron">
-    <div class="container">
-        <!--<h1>Hello, world!</h1>
-        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-        <p><a class="btn btn-primary btn-lg" role="button">Learn more &raquo;</a></p>
-    	</div>-->	
-    	<?php
-			foreach ($resultat as $key) {
-				//echo $key['title'].','.$key['pseudo'].','.$key['detail'];
-			?>
-				<table id="tableau" class="table">
-					<tr>
-							<?php if (isset($key['title'])):?>
-						<td><strong>Titre</strong></td>
-							<?php endif?>
-							<?php if (isset($key['pseudo'])):?>
-						<td><strong>Pseudo</strong></td>
-							<?php endif?>
-							<?php if (isset($key['detail'])):?>
-						<td><strong>Details</strong></td>
-							<?php endif?>
-					</tr>
-					<tr>	<?php if (isset($key['title'])):?>
-						<td><?php echo $key['title'];?></td>
-							<?php endif?>
-							<?php if (isset($key['pseudo'])):?>
-						<td><?php echo $key['pseudo'];?></td>
-							<?php endif?>
-							<?php if (isset($key['detail'])):?>
-						<td><?php echo $key['detail'];?></td>
-							<?php endif?>
-					</tr>
-				</table>
-
-<!--<div class="container">
-    
-    <div class="row">
-        <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-md-4">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-    </div>
-
-    <hr>
-
-    <footer>
-        <p>&copy; Company 2014</p>
-    </footer>
-</div> /container
-
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="../../dist/js/bootstrap.min.js"></script>
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-<script src="./bootstrap/js/bootstrap.min.js"></script>
-<script src="./bootstrap/js/jquery-1.8.3.min.js"></script>
-<script type="text/javascript" src="app-jquery.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="../../dist/js/bootstrap.min.js"></script>
+	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+	<script src="./bootstrap/js/bootstrap.min.js"></script>
+	<script src="./bootstrap/js/jquery-1.8.3.min.js"></script>
+	<script type="text/javascript" src="app-jquery.js"></script>
+			
 		
-	</body>
 </html>
-			<?php
-			}
-			?>
+			
