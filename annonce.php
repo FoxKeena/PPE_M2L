c7201b71122e42884109afb177f1a925090aed91
Keena <Keena@MacBook-Pro-de-Keena.local>
2015-03-02 16:21:56 +0100

    modal new add

--- annonce.php
+++ annonce.php
@@ -23,7 +23,7 @@ include('navbar.html');?>
             <div class="jumbotron">
                 <h1>Créer son annonce</h1>
                 <p>Vous aussi vous pouvez créer votre propre annonce!</p>
-                <p><a class="btn btn-success" href="Adds/post_add.php">Cliquez ici!</a></p>
+                <p> <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal_add">Cliquez Ici!</button></p>
             </div>
             <div class="row">
                 <?php foreach($add as $new_add): ?>
@@ -37,6 +37,65 @@ include('navbar.html');?>
         </div>
         </div>
     </div>
+<!-- Modal Nouvel Add -->
+<div class="modal fade bs-example-modal-sm" id="modal_add" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span> </button>
+                <h4 class="modal-title">Nouvelle Annonce</h4>
+            </div>
+            <div class="modal-body">
+                <form method="post">
+                <div class="form-group">
+
+                        <input type="text" placeholder="Titre" class="form-control">
+                </div>
+                <div class="form-group">
+                    <select class="form-control">
+                        <optgroup label="Sports de Combat">
+                            <option>Aikido</option>
+                            <option>Boxe Anglaise</option>
+                            <option>Escrime</option>
+                            <option>Judo</option>
+                            <option>Karaté</option>
+                        </optgroup>
+                        <optgroup label="Sports Aquatiques">
+                                    <option>Natation</option>
+                                    <option>Canoë Kayak</option>
+                                    <option>Aviron</option>
+                        </optgroup>
+                        <optgroup label="Sports de Balles">
+                                    <option>Rugby</option>
+                                    <option>Tennis</option>
+                                    <option>VolleyBall</option>
+                                    <option>Handball</option>
+                                    <option>Bowling</option>
+                                    <option>Badmington</option>
+                                    <option>Baseball</option>
+                                    <option>BasketBall</option>
+                                    <option>Golf</option>
+                        </optgroup>
+                        <optgroup label="Sports de Coordination">
+                                    <option>Gymnastique</option>
+                                    <option>Echecs</option>
+                                    <option>Danse</option>
+                                    <option>Athléthisme</option>
+                                    <option>Escalade</option>
+                        </optgroup>
+                    </select>
+                    </div>
+                <div class="form-group">
+                        <textarea class="form-control" rows="3">Annonce</textarea>
+                </div>
+                    </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary">Envoyer</button>
+            </div>
+        </div>
+    </div>
+</div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="bootstrap/js/bootstrap.min.js"></script>
 <script src="js/function.js"></script>
